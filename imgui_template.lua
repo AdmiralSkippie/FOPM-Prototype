35a89be964e0bb4f4fd40c3148393cc4a8862af4
AdmiralSkippie <sebcamleal4509@gmail.com>
2026-05-01 10:56:48 -0500

    Starting to Modify for testing porpouses

--- imgui_template.lua
+++ imgui_template.lua
@@ -13,9 +13,9 @@ if not SUPPORTS_FLOATING_WINDOWS then
 end
 -----------------------------------Variables go here--------------------------------------------
 --Set you variables here, datarefs, etc...
-
-
-
+local WND_SETTINGS = false
+local WND_MAIN = true
+local WND_BREAFING = false
 
 
 
@@ -26,13 +26,115 @@ end
 -------------------------------------Build Your GUI Here----------------------------------------
 
 function myProgram_on_build(myProgram_wnd, x, y)  --<-- your GUI code goes in this section.
-  
-
-
-
-
-  
-end -- function myProgram_on_build
+    if WND_MAIN then
+        if imgui.SmallButton("Settings") then
+            WND_SETTINGS = true
+            WND_MAIN = false
+            WND_BREAFING = false
+        end
+        imgui.SameLine()
+        if imgui.SmallButton("Breafing") then
+            WND_SETTINGS = false
+            WND_MAIN = false
+            WND_BREAFING = true
+        end
+        imgui.Separator()
+        imgui.Spacing()
+        imgui.TextUnformatted("FLT Phase:")
+        imgui.SameLine()
+        imgui.TextUnformatted("TXT_PHASE")
+        imgui.Spacing()
+        imgui.Separator()
+        imgui.Spacing()
+        -- CHECKLIST
+        if PREFLIGHT then
+            if not BS_DTL and not EX_BS_DTL and not EX_SEC_CL and PF_DONE then
+                if imgui.SmallButton("Before Start CKL") then
+                    EX_BS_DTL = true
+                end
+            end
+            imgui.SameLine()
+            if not SEC_CL and not EX_SEC_CL and not BS_DTL and not EX_BS_DTL then
+                if imgui.SmallButton("Securing CKL") then
+                    EX_SEC_CL = true
+                end
+            end
+            if BS_DTL and not EX_BS_CL then
+                if imgui.SmallButton("Before Start CKL BTL") then
+                    EX_BS_CL = true
+                end
+            end
+        end
+        if PUSHBACK then
+            if not AS_CL and not EX_AS_CL and AS_PROC_DONE then
+                if imgui.SmallButton("After Start CKL") then
+                    EX_AS_CL = true
+                end
+            end
+        end
+        if TAXI_OUT then
+            if not BTO_DTL and not EX_BTO_DTL and BTO_PROC_DONE then
+                if imgui.SmallButton("Before Takeoff CKL") then
+                    EX_BTO_DTL = true
+                end
+            end
+        end
+        -- PROCEDURES
+        imgui.Spacing()
+        imgui.Separator()
+        imgui.Spacing()
+        imgui.SmallButton("Before Start CKL")
+        imgui.SameLine()
+        imgui.SmallButton("Securing CKL")
+    end
+    if WND_BREAFING then
+        if imgui.SmallButton("Settings") then
+            WND_SETTINGS = true
+            WND_MAIN = false
+            WND_BREAFING = false
+        end
+        imgui.SameLine()
+        if imgui.SmallButton("Main") then
+            WND_SETTINGS = false
+            WND_MAIN = true
+            WND_BREAFING = false
+        end
+        imgui.Separator()
+        imgui.Spacing()
+        imgui.TextUnformatted("FLT Phase:")
+        imgui.SameLine()
+        imgui.TextUnformatted("TXT_PHASE")
+        imgui.Spacing()
+        imgui.Separator()
+        imgui.Spacing()
+        imgui.SmallButton("Before Start CKL")
+        imgui.SameLine()
+        imgui.SmallButton("Securing CKL")
+        imgui.Spacing()
+        imgui.Separator()
+        imgui.Spacing()
+        imgui.SmallButton("Before Start CKL")
+        imgui.SameLine()
+        imgui.SmallButton("Securing CKL")
+    end
+    if WND_SETTINGS then
+        if imgui.SmallButton("Main") then
+            WND_SETTINGS = false
+            WND_MAIN = true
+            WND_BREAFING = false
+        end
+        imgui.SameLine()
+        if imgui.SmallButton("Breafing") then
+            WND_SETTINGS = false
+            WND_MAIN = false
+            WND_BREAFING = true
+        end
+        imgui.Spacing()
+        imgui.Separator()
+        imgui.Spacing()
+        imgui.TextUnformatted("SETTINGS")
+    end
+end
 
 -------------------------------------------------------------------------------------------------
 
@@ -47,8 +149,11 @@ end -- function myProgram_on_build
 myProgram_wnd = nil  -- flag for the show_wnd set to nil so that creation below can happen - float_wnd_create
 
 function myProgram_show_wnd() -- This is called when user toggles window on/off, if the next toggle is for ON
-    myProgram_wnd = float_wnd_create(500, 500, 1, true)
-    float_wnd_set_title(myProgram_wnd, "myProgram IMGUI Template v1.0")
+    local posX = (SCREEN_WIDTH / 1.08) - (250 / 2)
+    local posY = (SCREEN_HEIGHT / 1.15) - (125 / 2)
+    myProgram_wnd = float_wnd_create(250, 125, 1, true)
+    float_wnd_set_title(myProgram_wnd, "FO/PM")
+    float_wnd_set_position(myProgram_wnd, posX, posY)
     float_wnd_set_imgui_builder(myProgram_wnd, "myProgram_on_build")
 end
 
@@ -88,7 +193,7 @@ end
 ----"add_macro" - adds the option to the FWL macro menu in X-Plane
 ----"create command" - creates a show/hide toggle command that calls the toggle_myProgram_window()
 
-add_macro("myProgram Menus: open/close", "myProgram_show_wnd()", "myProgram_hide_wnd()", "deactivate")
+add_macro("FO/PM", "myProgram_show_wnd()", "myProgram_hide_wnd()", "deactivate")
 create_command("myProgram_menus/show_toggle", "open/close myProgram Menu window", "toggle_myProgram_window()", "", "")
 
 --[[
